1238c09ea397a787130ef7a980cb6835389c20cd
Fabio <fabiofigueiredovr@bol.com.br>
2017-12-09 07:56:19 -0200

    Start

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -10,41 +10,55 @@ package com.parse.starter;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.GetCallback;
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    ParseObject pontuacao = new ParseObject("Pontuacao");
+    pontuacao.put("nome", "Maria");
+    pontuacao.put("pontos", 200);
+    pontuacao.saveInBackground(new SaveCallback() {
+      @Override
+      public void done(ParseException e) {
+        if(e == null){
+          //sem erro
+          Log.i("Salvar pontos", "Dados salvos com sucesso");
+        } else {
+          Log.i("Salvar pontos", "Erro ao salvar dados");
+        }
+      }
+    });
+
+//    ParseQuery<ParseObject> consulta = ParseQuery.getQuery("Pontuacao");
+//    consulta.getInBackground("u8VoyAaNW3", new GetCallback<ParseObject>() {
+//      @Override
+//      public void done(ParseObject object, ParseException e) {
+//        if(e == null){
+//          //sem erro
+//         object.put("pontos", 500);
+//         object.saveInBackground();
+//        } else {
+//          Log.i("consultaObjeto", "Erro ao consultar objeto");
+//        }
+//      }
+//    });
+
   }
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
-  }
 }
